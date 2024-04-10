83c85b1bf4fb3dba83f6e6f44aa98aa772125792
Davi Maia <121465789+DaviMaiaF@users.noreply.github.com>
2024-04-10 10:41:12 -0300

    Criação da função Calcular média e reestruturação da Struct
    Foi criada uma adaptação do primeiro commit, o deixando mais organizado.

--- main1.c
+++ main1.c
@@ -5,19 +5,23 @@
 #define MAX_NOME 50
 #define MAX_CURSO 20
 
+//Definição de Struct de dados
 struct pessoa {
     char nome[MAX_NOME];
     char telefone[20];
     char curso[MAX_CURSO];
+    float nota1;
+    float nota2;
 };
 
-struct notas {
-    float nota1, nota2;
-};
+//Função para calcular média dos alunos
+float calcularMedia(float nota1, float nota2) {
+	return (nota1 + nota2) / 2;
+}
 
 int main() {
     FILE *arquivo;
-    arquivo = fopen("C:/Users/gabriel.cezar/Downloads/DadosEntrada.csv", "r");
+    arquivo = fopen("C:/Users/davi.figueiredo/Documents/dados/DadosEntrada.csv", "r");
 
     if (arquivo == NULL) {
         printf("Impossivel abrir o arquivo.\n");
@@ -25,21 +29,21 @@ int main() {
     }
 
     struct pessoa Pessoa;
-    struct notas Notas;
     float total;
 
     char linha[200];
     fgets(linha, sizeof(linha), arquivo);
-
+    
+	//looping de saída das informações
     while (fgets(linha, sizeof(linha), arquivo) != NULL) {
-        sscanf(linha, "%[^,],%[^,],%[^,],%f,%f", Pessoa.nome, Pessoa.telefone, Pessoa.curso, &Notas.nota1, &Notas.nota2);
+        sscanf(linha, "%[^,],%[^,],%[^,],%f,%f", Pessoa.nome, Pessoa.telefone, Pessoa.curso, &Pessoa.nota1, &Pessoa.nota2);
         
-        total = (Notas.nota1 + Notas.nota2) / 2;
+        total = calcularMedia(Pessoa.nota1, Pessoa.nota2);
         printf("\nNome: %s", Pessoa.nome);
         printf("\nTelefone: %s", Pessoa.telefone);
         printf("\nCurso: %s", Pessoa.curso);
-        printf("\nNota 1: %.2f", Notas.nota1);
-        printf("\nNota 2: %.2f", Notas.nota2);
+        printf("\nNota 1: %.2f", Pessoa.nota1);
+        printf("\nNota 2: %.2f", Pessoa.nota2);
         printf("\nNota Final: %.2f\n", total);
     }
 
